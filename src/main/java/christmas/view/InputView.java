93d7f93646d1c7d5093fa5aaf7ba27af9ae1c5af
dradnats1012 <herjebi1012@gmail.com>
2023-11-14 08:23:25 +0900

    feat : InputView 입력클래스 생성

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,51 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.message.ConsoleMessage;
+import christmas.message.ErrorMessage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InputView {
+
+    public int inputDate() {
+
+        System.out.println(ConsoleMessage.WELCOME_EVENT_PLANNER);
+        System.out.println(ConsoleMessage.EXPECTED_VISIT_DATE.getMessage());
+        String input = Console.readLine();
+        int date;
+
+        try {
+            date = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE_DATE.getMessage());
+        }
+
+        return date;
+    }
+
+    public List<Map<String, Integer>> inputMenu() {
+
+        String menu = Console.readLine();
+        String[] menus = menu.split(",");
+        List<Map<String, Integer>> menuList = new ArrayList<>();
+
+        for (String menuPair : menus) {
+            String[] menuPairSplit = menuPair.split("-");
+            Map<String, Integer> menuMap = new HashMap<>();
+
+            try {
+                menuMap.put(menuPairSplit[0], Integer.parseInt(menuPairSplit[1]));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.getMessage());
+            }
+
+            menuList.add(menuMap);
+        }
+
+        return menuList;
+    }
+}
